d1f6b3d5269c70056caac61d16eb4b725daecc1d
Mitsu Cn <mitsucn@Mitsus-MacBook-Pro.local>
2021-03-25 13:47:39 -0400

    day-4

--- day-4/dbapp/app.js
+++ day-4/dbapp/app.js
@@ -2,14 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var cors = require('cors');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var superheroesRouter = require('./routes/superheroes');
 
 var app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
