a86c4f34b75ac0a4c04b8e66e9e3ab82ebcd1056
Aakash Malhotra <aakash@koverhoop.com>
2021-03-31 12:18:19 -0400

    day-8

--- day-8/frontend/script.js
+++ day-8/frontend/script.js
@@ -14,7 +14,7 @@ function createAccount() {
             // API was successful
             alert("user created");
         }).fail((xhrObj, textStatus) => {
-            // API sends an error . Rrsponse code in 400s or 500s
+            // API sends an error . Response code in 400s or 500s
             if (xhrObj && xhrObj.responseJSON && xhrObj.responseJSON.message)
                 alert(xhrObj.responseJSON.message);
         });
